cbbc34c704f74cb4eabd54838b47623b046247dd
Nitya <22bch036@nith.ac.in>
2025-05-17 16:46:46 +0530

    working with @DeleteMapping

--- src/main/java/com/api/book/bootrestbook/controllers/services/BookService.java
+++ src/main/java/com/api/book/bootrestbook/controllers/services/BookService.java
@@ -2,6 +2,7 @@ package com.api.book.bootrestbook.controllers.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
@@ -34,5 +35,10 @@ public class BookService {
     public Book addBook(Book b){
         list.add(b);
         return b;
+    }
+
+    //delete book
+    public void deleteBook(int bid){
+        list= list.stream().filter(book->book.getId()!=bid).collect(Collectors.toList());
     }
 }
